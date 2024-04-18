ef469b52a2074114af0d1fa8dd54f7e09473a2d2
utente <utente@MacBook-Air-di-utente.local>
2024-04-18 19:24:20 +0200

    fix attempt 2 for #1

--- backend/http/handler.py
+++ backend/http/handler.py
@@ -3,21 +3,22 @@ import glob
 import os
 
 class mHandler(http.server.SimpleHTTPRequestHandler):
-    main_path = '/'.join(__file__.split('/')[:-3])
-    http_path = main_path + '/interface'
+    slash = '\\' if os.name == 'nt' else '/'
+    main_path = slash.join(__file__.split('/')[:-3])
+    http_path = main_path + slash + 'interface'
     allowed = []
     def log_message(self, format, *args):
         pass
 
     def get_allowed_paths(self):
         if not self.allowed:
-            for path in glob.glob(self.http_path + '/**', recursive=True):
+            for path in glob.glob(self.http_path + self.slash + '**', recursive=True):
                 self.allowed.append(path[len(self.http_path):])
         return self.allowed
 
     def translate_path(self, path: str) -> str:
         if path in self.get_allowed_paths():
             return self.http_path + path
-        if path == '/data.json':
-            return self.main_path + '/data.json'
+        if path == self.slash + 'data.json':
+            return self.main_path + self.slash + 'data.json'
         return ''
